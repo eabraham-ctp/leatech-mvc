281788d2d4d129ffa15334a56409f7bfc702c90f
Jeff Dickman <jeff.dickman@cloudtp.com>
2017-09-22 19:06:37 -0600

    Final configs to get Consul and Vault up and running.

--- components/pcs_network/terraform/backend.tf
+++ components/pcs_network/terraform/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "svbrnd-pcs-dev-config"
-    key    = "terraform/aws/pcs/vpc/tfstate/terraform.tfstate"
-    region = "us-west-2"
-    encrypt = "true"
-  }
+ backend "consul" {
+   path = "aws/pcs/vpc/tfstate"
+ }
 }
