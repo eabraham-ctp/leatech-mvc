4d55ca70605e463e192448a237dcd2f7c3eeb09f
vmendoza <vmendoza@gmail.com>
2017-10-11 09:26:09 -0700

    FEATURE/Cloudtrail - adding code removed
    Signed-off-by: vmendoza <vmendoza@gmail.com>

--- bootstrap/build/include/create_general_kms.sh
+++ bootstrap/build/include/create_general_kms.sh
@@ -18,10 +18,6 @@ function create_general_kms () {
 		KMS_GENERAL_KEY=$(terraform output pcs_general_kms | grep  '[0-9a-z]*-[0-9a-z]*-[0-9a-z]*-[0-9a-z]*-[0-9a-z]*' 2> /dev/null)
 		KMS_AMI_KEY=$(terraform output pcs_ami_kms | grep '[0-9a-z]*-[0-9a-z]*-[0-9a-z]*-[0-9a-z]*-[0-9a-z]*' 2> /dev/null)
 
-		export TF_VAR_kms_ami_key=$(terraform output pcs_ami_kms)
-    export TF_VAR_kms_general_key=$(terraform output pcs_general_kms)
-    export TF_VAR_kms_cloudtrail_key=$(terraform output pcs_cloudtrail_kms)
-
 		if [[ -z $KMS_GENERAL_KEY ]] || [[ -z $KMS_AMI_KEY ]]; then
 
 			terraform init
@@ -42,6 +38,11 @@ function create_general_kms () {
 			terraform output | tee -a $OUTPUTS_FILE
 
 			export KMS_AMI_KEY=$(terraform output pcs_ami_kms)
+
+			export TF_VAR_kms_ami_key=$(terraform output pcs_ami_kms)
+      export TF_VAR_kms_general_key=$(terraform output pcs_general_kms)
+      export TF_VAR_kms_cloudtrail_key=$(terraform output pcs_cloudtrail_kms)
+
 		fi
 		cd $SCRIPT_DIR
 	else
